49442512e2dd84f9480dc98e1218b8128200c20c
Umutoni123-aa <u.nada@alustudent.com>
2025-11-01 14:05:01 +0000

    commit on alubackend api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,24 +8,22 @@ import sys
 """Module"""
 
 if __name__ == '__main__':
-user_id = sys.argv[1]
-user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-.format(user_id)
-todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-.format(user_id)
+    user_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(user_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(user_id)
 
-user_info = requests.request('GET', user_url).json()
-todos_info = requests.request('GET', todos_url).json()
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
 
-employee_name = user_info["name"]
-task_completed = list(filter(lambda obj:
-(obj["completed"] is True), todos_info))
-number_of_done_tasks = len(task_completed)
-total_number_of_tasks = len(todos_info)
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
 
-print("Employee {} is done with tasks({}/{}):".
-format(employee_name, number_of_done_tasks, total_number_of_tasks))
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
 
-[print("\t " + task["title"]) for task in task_completed]
-
-
+    [print("\t " + task["title"]) for task in task_completed]
